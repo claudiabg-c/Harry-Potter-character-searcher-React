3b4ccb2260be0f02843c8aebe163c90af12ae066
Claudia <claudia.berrocalgomez@gmail.com>
2022-01-25 15:29:23 +0100

    add fill image

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,25 @@
+import logo from "../images/hp-logo.png";
+
 function CharacterCard(props) {
   const renderCharacters = () => {
     return props.characters.map((character, index) => {
-      return (
-        <li key={index}>
-          <img src={character.img} alt={`Foto de ${character.name}`} />
-          <h2>{character.name}</h2>
-          <p>{character.species}</p>
-        </li>
-      );
+      if (character.img !== "") {
+        return (
+          <li key={index}>
+            <img src={character.img} alt={`Foto de ${character.name}`} />
+            <h2>{character.name}</h2>
+            <p>{character.species}</p>
+          </li>
+        );
+      } else {
+        return (
+          <li key={index}>
+            <img src={logo} alt={`Foto del personaje no disponible`} />
+            <h2>{character.name}</h2>
+            <p>{character.species}</p>
+          </li>
+        );
+      }
     });
   };
   return <>{renderCharacters()}</>;

ec8b4d06963a84f4703f54acd152045c93ced4eb
Claudia <claudia.berrocalgomez@gmail.com>
2022-01-27 13:19:31 +0100

    fix bug render

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,12 +3,12 @@ import logo from "../images/hp-logo.png";
 import "../styles/CharacterDetail.scss";
 
 function CharacterDetail(props) {
-  let img;
-  if (props.character.img === "") {
-    img = false;
-  } else {
-    img = true;
-  }
+  // let img;
+  // if (props.character.img === "") {
+  //   img = false;
+  // } else {
+  //   img = true;
+  // }
 
   const getStatus = () => {
     return props.character.status === true ? (
@@ -38,45 +38,60 @@ function CharacterDetail(props) {
     }
   };
 
-  const getNickNames = props.character.nickName.map((eachNickName) => {
-    return " " + eachNickName;
-  });
+  const getNickNames = () => {
+    return props.character.nickName.map((eachNickName) => {
+      return " " + eachNickName;
+    });
+  };
+
+  const render = () => {
+    let img;
+    if (props.character.img === "") {
+      img = false;
+    } else {
+      img = true;
+    }
+    return img;
+  };
 
   return (
     <div className="characterbody">
       <Link to="/" className={`link ${props.characterHouse}`}>
         <i className="fas fa-arrow-left"></i> Volver al listado
       </Link>
-
-      <section className={`sectiondetail ${props.characterHouse}background`}>
-        <img
-          src={img ? `${props.character.img}` : `${logo}`}
-          alt={
-            img
-              ? `Foto de ${props.character.name}`
-              : `Foto del personaje no disponible`
-          }
-          className={`characterimg ${props.characterHouse}`}
-        />
-        <section className={`charactercontent ${props.characterHouse}color`}>
-          <h1>{props.character.name}</h1>
-          <article className="details">
-            <p>Estatus: {getStatus()}</p>
-            <p>Especie: {getSpecies()}</p>
-            <p>Género: {getGender()}</p>
-            <p>
-              Casa:{" "}
-              {props.characterHouse.charAt(0).toUpperCase() +
-                props.characterHouse.slice(1)}
-            </p>
-            <p>
-              {props.character.nickName.length === 0
-                ? null
-                : `Otros nombres: ${getNickNames}`}
-            </p>
-          </article>
+      {props.character !== undefined ? (
+        <section className={`sectiondetail ${props.characterHouse}background`}>
+          <img
+            src={render() ? `${props.character.img}` : `${logo}`}
+            alt={
+              render()
+                ? `Foto de ${props.character.name}`
+                : `Foto del personaje no disponible`
+            }
+            className={`characterimg ${props.characterHouse}`}
+          />
+          <section className={`charactercontent ${props.characterHouse}color`}>
+            <h1>{props.character.name}</h1>
+            <article className="details">
+              <p>Estatus: {getStatus()}</p>
+              <p>Especie: {getSpecies()}</p>
+              <p>Género: {getGender()}</p>
+              <p>
+                Casa:{" "}
+                {props.characterHouse.charAt(0).toUpperCase() +
+                  props.characterHouse.slice(1)}
+              </p>
+              <p>
+                {props.character.nickName.length === 0
+                  ? null
+                  : `Otros nombres: ${getNickNames()}`}
+              </p>
+            </article>
+          </section>
         </section>
-      </section>
+      ) : (
+        "Buscando..."
+      )}
     </div>
   );
 }

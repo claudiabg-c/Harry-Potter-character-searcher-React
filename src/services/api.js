e4826f3fe329643b7dfafc46b2e03e6d0e7e196f
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2025-03-19 22:49:36 +0100

    Move characters.json to public/services and update callToApi to use fetch for data retrieval

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,20 @@
-import characters from './characters.json';
-
 const callToApi = (searchHouse) => {
-  return new Promise((resolve) => {
-    const result = characters
-      .filter((character) => character.house.toLowerCase() === searchHouse.toLowerCase())
-      .map((character) => ({
-        img: character.image,
-        name: character.name,
-        species: character.species,
-        status: character.alive,
-        gender: character.gender,
-        nickName: character.alternate_names,
-        houseOf: character.house.toLowerCase(),
-      }));
-
-    resolve(result);
-  });
+  return fetch(process.env.PUBLIC_URL + '/services/characters.json')
+    .then((response) => response.json())
+    .then((characters) => {
+      const result = characters
+        .filter((character) => character.house.toLowerCase() === searchHouse.toLowerCase())
+        .map((character) => ({
+          img: character.image,
+          name: character.name,
+          species: character.species,
+          status: character.alive,
+          gender: character.gender,
+          nickName: character.alternate_names,
+          houseOf: character.house.toLowerCase(),
+        }));
+      return result;
+    });
 };
 
 export default callToApi;
